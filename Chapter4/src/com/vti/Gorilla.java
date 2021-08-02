2454e5794c925a301073b90942d4557199f32e46
VTI\duc.nguyenviet <nguyenvietduc030496@gmail.com>
2021-08-02 10:23:11 +0700

    add folder source for chapter 4

---
+++ Chapter4/src/com/vti/Gorilla.java
@@ -0,0 +1,22 @@
+package com.vti;
+
+public interface Gorilla {
+	String move();
+}
+
+class GorillaFamily {
+	String walk = "walk";
+
+	void everyonePlay(boolean baby) {
+		String approach = "amble";
+//		approach = "run";
+
+		play(() -> walk);
+		play(() -> baby ? "hitch a ride" : "run");
+		play(() -> approach);
+	}
+
+	void play(Gorilla g) {
+		System.out.println(g.move());
+	}
+}
