83d063a38b8ced26d0f1c83b03da247954be400d
VTI\duc.nguyenviet <nguyenvietduc030496@gmail.com>
2021-08-03 11:49:47 +0700

    bonus source exam test udemy

---
+++ Chapter3/src/com/vti/exam/udemy/Test6.java
@@ -0,0 +1,26 @@
+package com.vti.exam.udemy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author duc.nguyenviet
+ *
+ * Question 20
+ */
+public class Test6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list1 = new ArrayList<>();
+		list1.add("A");
+		list1.add("B");
+
+		List<? extends Object> list2 = list1;
+		list2.remove("A"); // Line 13
+//		list2.add("C"); // Line 14 compiler error
+
+		System.out.println(list2);
+	}
+
+}
---
+++ Chapter3/src/com/vti/exam/udemy/Test7.java
@@ -0,0 +1,27 @@
+package com.vti.exam.udemy;
+
+/**
+ * @author duc.nguyenviet
+ *
+ * Question 22
+ */
+public class Test7 {
+
+	public static void main(String[] args) {
+		Operator<String> operator = (s1, s2) -> s1 + s2; 
+		System.out.println(operator);
+		
+		System.out.println((Operator<String>)(String s1, String s2) -> s1 + s2);
+
+		System.out.println(new Operator<String>() {
+			public String operation(String s1, String s2) {
+				return s1 + s2;
+			}
+		});
+	}
+
+}
+
+interface Operator<T> {
+	public abstract T operation(T t1, T t2);
+}
