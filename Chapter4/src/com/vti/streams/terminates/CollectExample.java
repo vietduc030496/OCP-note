d983b53dc9fd23c27ff748d47b7440dfad0b9259
vietduc030496 <nguyenvietduc030496@gmail.com>
2021-08-17 09:11:45 +0700

    add example collect method in stream

--- Chapter4/src/com/vti/streams/terminates/CollectExample.java
+++ Chapter4/src/com/vti/streams/terminates/CollectExample.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 
 public class CollectExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		Stream<String> stream = Stream.of("w", "o", "l", "f");
 		StringBuilder word = stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
 		System.out.println(word.toString()); // wolf
