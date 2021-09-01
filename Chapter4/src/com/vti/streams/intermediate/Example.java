8caea0289e2d47e7a58a0406c7608e4c867bdcee
vietduc030496 <nguyenvietduc030496@gmail.com>
2021-09-01 10:09:33 +0700

    bonus example

--- Chapter4/src/com/vti/streams/intermediate/Example.java
+++ Chapter4/src/com/vti/streams/intermediate/Example.java
@@ -11,8 +11,15 @@ public class Example {
 		
 		// distinct()
 		Stream<String> s2 = Stream.of("duck", "duck", "duck", "goose");
-		s2.distinct().forEach(System.out::println); // duckgoose
+		s2.distinct().forEach(System.out::print); // duckgoose
 		
+		// limit() and skip()
+		Stream<Integer> s3 = Stream.iterate(1, n -> n + 1);
+		s3.skip(5).limit(2).forEach(System.out::print); // 67
+		
+		// map
+		Stream<String> s4 = Stream.of("monkey", "gorilla", "bonobo");
+		s4.map(String::length).forEach(System.out::print); // 676
 		
 	}
 
